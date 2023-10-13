41522b0ecf3c88a69cac75fc2323b44b13737359
ganeshjatms@gmail.com <ganeshjatms@gmail.com>
2023-10-13 13:14:15 +0530

    new php code

--- index.php
+++ index.php
@@ -1,52 +1,51 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>PHP Login System</title>
-</head>
-<body>
-    <h1>PHP Login System</h1>
+<?php 
+ob_start();
+session_start();
+include('inc/header.php');
+include 'Inventory.php';
+$inventory = new Inventory();
+$inventory->checkLogin();
+?>
+<script src="js/jquery.dataTables.min.js"></script>
+<script src="js/dataTables.bootstrap.min.js"></script>		
+<link rel="stylesheet" href="css/dataTables.bootstrap.min.css" />
+<script src="js/common.js"></script>
+<?php include('inc/container.php');?>
+<div class="container">		
+	<?php include("menus.php"); ?>   
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="card card-default rounded-0 shadow">
+				<div class="card-header">
+					<div class="row">
+						<div class="col-lg-10 col-md-10 col-sm-8 col-xs-6">
+							<h3 class="card-title">Inventory</h3>
+						</div>						
+					</div>
+				</div>
+				<div class="card-body">
+					<div class="row"><div class="col-sm-12 table-responsive">
+						<table id="inventoryDetails" class="table table-bordered table-striped">
+							<thead><tr>
+								<th>#</th>      
+								<th>Product/Code</th>      
+								<th>Starting Inventory</th> 
+								<th>Inventory Recieved</th> 									
+								<th>Inventory Shipped</th>
+								<th>Inventory on Hand</th>								
+							</tr></thead>
+						</table>
+					</div></div>
+				</div>
+			</div>
+		</div>
+	</div>
+		
+</div>	
+<?php include('inc/footer.php');?>
 
-    <?php
-    session_start();
 
-    // Check if the user is already authenticated
-    if (isset($_SESSION['user_id'])) {
-        echo "Welcome, " . $_SESSION['user_id'] . "!<br>";
-        echo '<a href="logout.php">Logout</a>';
-    } else {
-        // Display the login form
-        if (isset($_POST['login'])) {
-            // Replace this with your actual user authentication logic
-            $username = $_POST['username'];
-            $password = $_POST['password'];
 
-            // For demonstration purposes, we are using a hard-coded user
-            $validUser = ($username === 'demo' && $password === 'password');
 
-            if ($validUser) {
-                $_SESSION['user_id'] = $username;
-                echo "Login successful. Welcome, " . $_SESSION['user_id'] . "!<br>";
-                echo '<a href="logout.php">Logout</a>';
-            } else {
-                echo "Invalid username or password. Please try again.<br>";
-            }
-        }
-    }
-    ?>
 
-    <form method="POST" action="">
-        <label for="username">Username:</label>
-        <input type="text" name="username" id="username" required><br>
 
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password" required><br>
-
-        <input type="submit" name="login" value="Login">
-    </form>
-
-    <br>
-
-    <!-- SSO Login Button (Sample) -->
-    <a href="sso-login.php">Login with SSO</a>
-</body>
-</html>
