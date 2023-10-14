e5563ef2caacc8972b0555fa8cf4fa3d2b1bdbf9
Ganesh-DevOps-Eng <ganeshjatms@gmail.com>
2023-10-14 09:32:02 +0530

    Create successful-sso-login.php

---
+++ successful-sso-login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Successful SSO Login</title>
+</head>
+<body>
+    <h1>Successful SSO Login</h1>
+    <p>You have successfully logged in using SSO.</p>
+    <p>Welcome to our application!</p>
+
+    <!-- Add any additional content or links as needed -->
+
+    <a href="/your-home-page.php">Go to Your Home Page</a>
+</body>
+</html>
