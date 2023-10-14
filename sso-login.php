13a7c69ac658da1624054ceb61c9cf01751255fc
Ganesh-DevOps-Eng <ganeshjatms@gmail.com>
2023-10-14 08:39:37 +0530

    Update sso-login.php

--- sso-login.php
+++ sso-login.php
@@ -1,37 +1,56 @@
 <?php
-require_once 'vendor/autoload.php'; // Include the OneLogin toolkit
 
-$azureADSettings = array(
-    'strict' => true,
-    'sp' => array(
-        'entityId' => 'https://matelliocorp-eb-env.eba-vwb8qadf.us-east-1.elasticbeanstalk.com/remote/saml/metadata/', // Your Service Provider Entity ID
-        'assertionConsumerService' => array(
-            'url' => 'https://matelliocorp-eb-env.eba-vwb8qadf.us-east-1.elasticbeanstalk.com/remote/saml/login/', // Your application's SSO callback URL
-            'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-        ),
-        'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
-        'x509cert' => '', // Your SP x509 certificate (leave empty if not used)
-        'privateKey' => '', // Your SP private key (leave empty if not used)
-    ),
-    'idp' => array(
-        'entityId' => 'https://sts.windows.net/4c1a87da-93b7-4e87-b5ad-da86a9c2ae72/',
-        'singleSignOnService' => array(
-            'url' => 'https://login.microsoftonline.com/4c1a87da-93b7-4e87-b5ad-da86a9c2ae72/saml2',
-            'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-        ),
-        'x509cert' => '', // IdP x509 certificate (leave empty if not used)
-    ),
-);
+// Fill in your Azure AD (Active Directory) application details
+$appid = "fc935d73-7641-400b-a8ad-cc548a8dce42"; // Your Azure AD Application Client ID
+$tenantid = "4c1a87da-93b7-4e87-b5ad-da86a9c2ae72"; // Your Azure AD Tenant ID
+$secret = "YOUR_CLIENT_SECRET"; // Your Azure AD Application Client Secret (if required)
+$redirect_uri = 'https://matelliocorp-eb-env.eba-vwb8qadf.us-east-1.elasticbeanstalk.com/remote/saml/login/'; // Your SAML Assertion Consumer Service URL
 
-$samlAuth = new OneLogin_Saml2_Auth($azureADSettings);
+$login_url = 'https://login.microsoftonline.com/' . $tenantid . '/saml2';
 
-if (!$samlAuth->isAuthenticated()) {
-    // Redirect the user to the IdP for authentication
-    $samlAuth->login();
-} else {
-    // User is authenticated, you can access user information with $samlAuth->getAttributes()
-    $username = $samlAuth->getAttributes()['NameID'][0];
-    // Store the user's information or perform further actions
-    // Redirect the user to your application's home page or other appropriate page
-    header('Location: /your-home-page.php');
+session_start();
+
+$_SESSION['state'] = session_id();
+
+echo '<h2><p>You can <a href="?action=login">Log In</a> with Microsoft</p></h2>';
+
+if (isset($_GET['action']) && $_GET['action'] == 'login') {
+    $params = array(
+        'client_id' => $appid,
+        'resource' => 'https://matelliocorp-eb-env.eba-vwb8qadf.us-east-1.elasticbeanstalk.com/remote/saml/metadata/',
+        'response_type' => 'id_token',
+        'response_mode' => 'form_post',
+        'scope' => 'openid',
+        'nonce' => bin2hex(random_bytes(16)), // Generate a unique nonce value
+        'state' => $_SESSION['state'],
+        'redirect_uri' => $redirect_uri
+    );
+
+    header('Location: ' . $login_url . '?' . http_build_query($params));
+}
+
+if (array_key_exists('id_token', $_POST)) {
+    // Retrieve the ID token from the POST data
+    $id_token = $_POST['id_token'];
+
+    // You can verify and decode the ID token to extract user information here
+
+    // Example: Decode and extract claims
+    $decoded_id_token = jwt_decode($id_token);
+    $user_name = $decoded_id_token['name'];
+    $user_email = $decoded_id_token['email'];
+
+    echo 'Welcome, ' . $user_name . '! Your email is ' . $user_email;
+}
+
+// You would typically include a library for decoding JWT tokens.
+// For example, you can use a library like Firebase JWT to decode the ID token.
+// Make sure to include and configure the JWT decoding library properly.
+
+// Example function for decoding JWT tokens using Firebase JWT (you'll need to include the library):
+function jwt_decode($token) {
+    require_once 'firebase-jwt/autoload.php'; // Include the library
+    $firebase = new \Firebase\JWT\JWT();
+
+    return (array) $firebase->decode($token, YOUR_PUBLIC_KEY, array('RS256'));
 }
