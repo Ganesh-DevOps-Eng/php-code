169022f6dd36cd8e92309d54421393fb6d7ed756
Ganesh-DevOps-Eng <ganeshjatms@gmail.com>
2023-10-14 09:39:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Login System</title>
+</head>
+<body>
+    <h1>PHP Login System</h1>
+
+    <?php
 session_start();
 
 if (isset($_SESSION['user_id'])) {
@@ -24,3 +32,20 @@ if (isset($_SESSION['user_id'])) {
     }
 }
 ?>
+
+    <form method="POST" action="">
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username" required><br>
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" required><br>
+
+        <input type="submit" name="login" value="Login">
+    </form>
+
+    <br>
+
+    <!-- SSO Login Button (Sample) -->
+    <a href="sso-login.php">Login with SSO</a>
+</body>
+</html>
